1e0c00126faa7378ea2be783117318be9b5ea3f0
DHANUSH BABU <royalbrothers1618@gmail.com>
2023-12-09 11:11:58 +0530

    updated chart.jsx

--- src/components/ChartCard.jsx
+++ src/components/ChartCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import {
     Chart as ChartJS,
@@ -25,12 +25,26 @@ ChartJS.register(
 const ChartCard = ({ arr = [], currency, days }) => {
     const prices = [];
     const date = [];
+    const [isLargeScreen, setIsLargeScreen] = useState(true);
 
     // Get color mode value
-    const ChartColor = useColorModeValue("teal.900", "rgba(129,230,217)")
+    const ChartColor = useColorModeValue("teal.900", "rgba(129,230,217)");
+
+    useEffect(() => {
+        // Check screen size on mount and resize
+        const checkScreenSize = () => {
+            setIsLargeScreen(window.innerWidth > 768); // Set breakpoint for large screens
+        };
+
+        window.addEventListener("resize", checkScreenSize);
+        checkScreenSize();
+
+        return () => window.removeEventListener("resize", checkScreenSize);
+    }, []);
 
     for (let i = 0; i < arr.length; i++) {
-        if (days === "24h") date.push(new Date(arr[i][0]).toLocaleTimeString());
+        if (days === "24h")
+            date.push(new Date(arr[i][0]).toLocaleTimeString());
         else date.push(new Date(arr[i][0]).toLocaleDateString());
         prices.push(arr[i][1]);
     }
@@ -46,13 +60,31 @@ const ChartCard = ({ arr = [], currency, days }) => {
         ],
     };
 
+    const aspectRatio = isLargeScreen ? true : false;
+
     return (
         <Line
             options={{
-                // responsive: true,
+                responsive: true,
+                maintainAspectRatio: aspectRatio,
                 elements: {
                     point: {
                         radius: 1,
+                    },
+                },
+                scales: {
+                    x: {
+                        ticks: {
+                            display: true,
+                            autoSkip: true,
+                            maxRotation: 10,
+                        },
+                    },
+                    y: {
+                        ticks: {
+                            display: true,
+                            autoSkip: true,
+                        },
                     },
                 },
             }}
